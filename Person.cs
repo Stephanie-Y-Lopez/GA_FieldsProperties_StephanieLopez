490ba47ae46cafe83a8533cf1009a989d176bf4e
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-01-10 12:45:56 -0800

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_FieldsProperties_StephanieLopez
+{
+    internal class Person
+    {
+        private string _name;
+        //Private fields are used to limit direct acess to certain data.
+        //And its role is to store the individual's name privately. 
+    }
+}
