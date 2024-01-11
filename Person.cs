80ac4f0e4addbd7157e0998dcf53634fb43325a6
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-01-11 13:00:38 -0800

    Step4done

--- Person.cs
+++ Person.cs
@@ -11,5 +11,15 @@ namespace GA_FieldsProperties_StephanieLopez
         private string _name;
         //Private fields are used to limit direct acess to certain data.
         //And its role is to store the individual's name privately. 
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+
+            // The need for using private fields is for  storing data that is not accessible from outside the class.
+            // And public properties provide controlled access to these fields by allowing encapsulation. 
+            // And the benefits if this exposition are, helping to maintain a clean orderly state of objects. Not allowing for unwanted modifications.
+        }
     }
 }
